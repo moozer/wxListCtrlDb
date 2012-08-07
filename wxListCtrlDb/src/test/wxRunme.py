26a35d5c7df19c81b85e5b805adb1826393f94fe
moz <moz@leon>
2012-08-07 22:11:10 +0200

    code cleanup

--- wxListCtrlDb/src/test/wxRunme.py
+++ wxListCtrlDb/src/test/wxRunme.py
@@ -10,7 +10,6 @@ references:
 from Gui import MyFrame
 import wx
 from test.SimpleDb import SimpleDb
-#from ListCtrlDb import InitListCtrl, ListCtrlInsert, ListCtrlUpdate
 
 class wxListCtrlTestFrame(MyFrame):
     def OnClickQuit(self, event):
@@ -21,21 +20,17 @@ class wxListCtrlTestFrame(MyFrame):
         for i in range(0, 10): #@UnusedVariable
             self._sql.InsertData()
 
-#        InitListCtrl( self._sql.GetAllData(), self.list_ctrl_1)
-#        ListCtrlInsert( self._sql.GetAllData(), self.list_ctrl_1)
         self.list_ctrl_1.InitFromDb(self._sql.GetAllData())
         self.list_ctrl_1.InsertFromDb(self._sql.GetAllData())
     
     def OnClickInsert(self, event):
         EntryId = self._sql.InsertData()
-#        ListCtrlInsert( self._sql.GetData( EntryId ), self.list_ctrl_1)
         self.list_ctrl_1.InsertFromDb(self._sql.GetData( EntryId ))
 
 
     def OnClickUpdate(self, event):
         EntryId = 5
         self._sql.UpdateData( EntryId )
-#        ListCtrlUpdate( self._sql.GetData( EntryId ), self.list_ctrl_1)
         self.list_ctrl_1.UpdateFromDb(self._sql.GetData( EntryId ))
 
 
